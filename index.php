598d5eb8b874f9c39e566fb7c4741f066d644998
elephant <elephant@localhost.localdomain>
2024-07-26 17:18:46 -0400

    Upgrade

--- index.php
+++ index.php
@@ -101,10 +101,10 @@
                     if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
                         echo "<h3>Requested Quote</h3>";
+                        echo "<h4>$sname Package</h4>";
 
                         if (isset($_POST['wd-qr-btn'])) {
 
-                            echo "<h4>$sname Package</h4>";
                             echo "<p>Get a comprehensive " . strtolower($sname) . " solution with our expert team!</p>";
                             echo "<strong>Package Includes</strong>";
                             echo "<ul><li>Full Stack Web Application development for a robust online presence.</li><li>Responsive Design to ensure seamless user experience across devices</li><li>Optimization for Search Engines to boost your online visibility.</li><li>Cyber Security measures to protect your website and data.</li><li>Maintenance and Update services to keep your website fresh and secure.</li></ul>";
@@ -117,7 +117,6 @@
                         
                         } else if (isset($_POST['gd-qr-btn'])) {
 
-                            echo "<h4>$sname Package</h4>";
                             echo "<p>Elevate your brand's visual identity with our expert " . strtolower($sname) . " services!</p>";
                             echo "<strong>Package Includes</strong>";
                             echo "<ul><li>Logos, Posters, Brochures, Menu Designs, and more.</li><li>Engaging motion graphics to capture your audience's attention.</li><li>Custom illustrations to bring your brand to life.</li><li>Eye-catching graphics to enhance your online presence.</li><li>Unique designs tailored to your brand's specific needs.</li></ul>";
@@ -130,7 +129,6 @@
                         
                         } else if (isset($_POST['ts-qr-btn'])) {
 
-                            echo "<h4>$sname Package</h4>";
                             echo "<p>Get peace of mind with our expert " . strtolower($sname) . " services!</p>";
                             echo "<strong>Package Includes</strong>";
                             echo "<ul><li>Fast and effective issue resolution.</li><li>Dedicated support tailored to your unique needs.</li><li>Regular checks to prevent issues before they arise.</li></ul>";
